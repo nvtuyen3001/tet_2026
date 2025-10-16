8a67b52bc86449ed8631f4fee7c0a8f39d980864
nvtuyen3001 <vantuyenthcs2k3@gmail.com>
2025-10-16 18:47:55 +0700

    fix: change @ alias imports to relative paths for Vercel build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import "@/App.css";
+import "./App.css";
 import axios from "axios";
 import YouTube from "react-youtube";
 import { Play, Pause, SkipBack, SkipForward, List, Calendar } from "lucide-react";
